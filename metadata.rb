03b9c88ae3bb141bced93687731d592075d0c29e
Kieren Evans <kevans@inviqa.com>
2016-06-13 21:41:37 +0100

    Add issues_url and source_url to metadata (foodcritic fixes).

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,10 @@ name 'package-replace'
 maintainer 'Inviqa UK LTD'
 maintainer_email 'kevans@inviqa.com'
 license 'apachev2'
-description 'Installs/Configures package-replace'
-long_description 'Installs/Configures package-replace'
+description 'Upgrades packages where an upgrade path is not clear'
+long_description 'Uninstall a package and replace it immediately with a different \
+version, which would normally conflict'
 version '0.1.0'
+
+issues_url 'https://github.com/inviqa/chef-package-replace/issues/'
+source_url 'https://github.com/inviqa/chef-package-replace/'
